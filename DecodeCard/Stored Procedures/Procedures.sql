0017c4d20b049965595329c3a7a2a0abe09ce255
huangqiwei <dz2008hqw@163.com>
2018-02-07 13:40:17 +0800

    更新存储过程

--- DecodeCard/Stored Procedures/Procedures.sql
+++ DecodeCard/Stored Procedures/Procedures.sql
@@ -30,12 +30,14 @@ WITH   ENCRYPTION
 AS
 SET NOCOUNT ON --不返回计数
 Set ARITHABORT ON
+declare @zkzmbhbase64 varchar(50)
+select @zkzmbhbase64=[dbo].charEncode(@ZKZM_Number)
 SELECT e.记录编号,e.错误编号,CONVERT(varchar(10),[dbo].dateDecode(e.出错时间),8) as 出错时间,[dbo].charDecode(ed.扣分类型) as 扣分类型,[dbo].intDecode(ed.扣除分数) as 扣除分数 FROM ErrorRecords e,StudentInfo s,ErrorData ed
 WHERE e.准考证明编号=s.准考证明编号 
 AND e.错误编号=ed.错误编号
 AND e.考试次数=s.考试次数 
 AND e.当日次数=s.当日次数 
-AND e.准考证明编号=[dbo].charEncode(@ZKZM_Number)
+AND e.准考证明编号=@zkzmbhbase64
 ORDER BY 记录编号 ASC
 /*2014-07-08 ZSZ 参数:准考证明编号 */
 
@@ -77,19 +79,21 @@ declare @DrcsN tinyint --当日次数
 declare @KSCJ int --成绩
 declare @ErrorCode int --错误编号
 declare @ErrorCodejl int --记录编号
-SELECT @Status=状态,@KscsN=考试次数,@DrcsN=当日次数 FROM Studentinfo WHERE 准考证明编号=[dbo].charEncode(@ZKZM_Number)
+declare @zkzmbhbase64 varchar(50)
+SELECT @zkzmbhbase64=[dbo].charEncode(@ZKZM_Number)
+SELECT @Status=状态,@KscsN=考试次数,@DrcsN=当日次数 FROM Studentinfo WHERE 准考证明编号=@zkzmbhbase64
 SELECT @KSCJ=100-SUM([dbo].intDecode(扣除分数)) FROM ErrorRecords JOIN ErrorData ON  ErrorRecords.错误编号=ErrorData.错误编号 
-WHERE ErrorRecords.准考证明编号=[dbo].charEncode(@ZKZM_Number) and 考试次数=@KscsN and 当日次数=@DrcsN
+WHERE ErrorRecords.准考证明编号=@zkzmbhbase64 and 考试次数=@KscsN and 当日次数=@DrcsN
 
 set @intErrorCode = 0
-if exists(Select TOP 1 错误编号 from ErrorRecords where 准考证明编号=[dbo].charEncode(@ZKZM_Number) and 记录编号>@Ejlbh and 当日次数=@DrcsN and 考试次数=@KscsN)
+if exists(Select TOP 1 错误编号 from ErrorRecords where 准考证明编号=@zkzmbhbase64 and 记录编号>@Ejlbh and 当日次数=@DrcsN and 考试次数=@KscsN)
 begin --1b
-Select TOP 1 @ErrorCode=错误编号,@ErrorCodejl=记录编号 from ErrorRecords where 准考证明编号=[dbo].charEncode(@ZKZM_Number) and 记录编号>@Ejlbh and 当日次数=@DrcsN and 考试次数=@KscsN
+Select TOP 1 @ErrorCode=错误编号,@ErrorCodejl=记录编号 from ErrorRecords where 准考证明编号=@zkzmbhbase64 and 记录编号>@Ejlbh and 当日次数=@DrcsN and 考试次数=@KscsN
 if exists(select * from errordata where 扣除分数!=[dbo].intEncode(0, RAND()) and 错误编号=@ErrorCode) 
 BEGIN --2b
 declare @counterrline int --统计错误行
 SELECT @counterrline=Count(*) FROM ErrorRecords er JOIN ErrorData ed ON  er.错误编号=ed.错误编号 
-WHERE er.准考证明编号=[dbo].charEncode(@ZKZM_Number) and 考试次数=@KscsN and 当日次数=@DrcsN and ed.扣除分数!=[dbo].intEncode(0, RAND()) and er.记录编号<=@Ejlbh
+WHERE er.准考证明编号=@zkzmbhbase64 and 考试次数=@KscsN and 当日次数=@DrcsN and ed.扣除分数!=[dbo].intEncode(0, RAND()) and er.记录编号<=@Ejlbh
 
 Select er.记录编号,[dbo].charDecode(ed.扣分类型) as 扣分类型,CONVERT(varchar(10),[dbo].dateDecode(er.出错时间),8) as 出错时间,[dbo].intDecode(ed.扣除分数) as 扣除分数,成绩=@KSCJ,ELine=@counterrline
 from ErrorRecords er JOIN ErrorData ed ON er.错误编号=ed.错误编号 
@@ -110,7 +114,7 @@ set @intErrorCode = 5
 goto ERR_HANDLER
 end --4e
 else if((@ErrorCode>201500 and  @ErrorCode<201700) OR (@ErrorCode>203500 and  @ErrorCode<203700) OR (@ErrorCode>204500 and  @ErrorCode<204700)OR (@ErrorCode>206500 and  @ErrorCode<206700) OR (@ErrorCode>207500 and  @ErrorCode<207700)
-OR (@ErrorCode>214500 and  @ErrorCode<214700)OR (@ErrorCode>215500 and  @ErrorCode<215700) OR (@ErrorCode>216500 and  @ErrorCode<216700))
+OR (@ErrorCode>214500 and  @ErrorCode<214700)OR (@ErrorCode>215500 and  @ErrorCode<215700) OR (@ErrorCode>216500 and  @ErrorCode<216700) OR @ErrorCode=10001)
 begin --5b
 Select er.记录编号,ed.错误编号,[dbo].charDecode(ed.扣分类型) as 扣分类型
 from ErrorRecords er JOIN ErrorData ed ON er.错误编号=ed.错误编号 
